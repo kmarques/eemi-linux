1da1e30445c754d6db11a742aab2049a065433fb
Karl MARQUES <karl@kmarques.dev>
2025-06-23 10:32:59 +0200

    shell scripting: structure

--- test.sh
+++ test.sh
@@ -34,10 +34,74 @@ echo \"$@\" arguments # arguments sous forme de tableau
 echo \"$*\" arguments # arguments sous forme de chaine
 echo $0
 echo $1 $2 $3 $4 # valeur indexé de l'argument
-shift
-echo $1 $2 $3 $4
-shift
-echo $1 $2 $3 $4
-shift
-echo $1 $2 $3 $4
-echo $? # code retour de la dernière commande
+#shift
+#echo $1 $2 $3 $4
+#shift
+#echo $1 $2 $3 $4
+#shift
+#echo $1 $2 $3 $4
+#echo $? # code retour de la dernière commande
+
+# Structure conditionnelle
+
+echo \# Structure IF
+if test "$1" = "toto"; then
+  echo "This is the best TOTO"
+elif [ "$1" -eq 7 ]; then
+  echo "This is Ronaldo" 
+else
+  echo "This is $1"
+fi
+
+echo \# Structure case/switch
+case $2 in
+  toto) echo "This is TOTO again" ;;
+  10) echo "This is Zizou" ;;
+  *) echo "This is $2" ;;
+esac
+
+echo \# Structure ternaire
+[ "$3" -lt 10 ] && echo "This is lower than expected" || echo "This is great"
+
+# Structure itérative
+USTENSILES=("couteau" "fourchette" "assiette")
+# for accumulateur
+echo \# for accumulateur
+for ((i=0; ${#USTENSILES[@]} - $i; i=$i+1)); do
+   echo ${USTENSILES[$i]}
+done
+
+# foreach
+echo \# foreach
+for ITEM in ${USTENSILES[@]}; do
+   echo $ITEM
+done
+
+# while
+echo \# while
+i=0
+while [ $((${#USTENSILES[@]} - $i)) -ne 0 ]; do
+  echo ${USTENSILES[$i]}
+  i=$((i+1))
+done
+
+# until
+echo \# until
+i=0
+until [ "$i" -eq ${#USTENSILES[@]} ]; do
+  echo ${USTENSILES[$i]}
+  i=$((i+1))
+done
+
+## Structure fonctionnelle
+function titi() {
+   local TUTU # déclare la variable que dans la fonction
+   TUTU=3
+   echo titi $1 $2 $TUTU
+   echo "J'ai ${#USTENSILES[@]} ustensiles"
+   return 123
+}
+
+titi coucou "gros minet"
+echo $?
+echo $TUTU
